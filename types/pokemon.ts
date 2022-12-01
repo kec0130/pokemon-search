b320cd4b9db4ac0397142629f0e6e0a0b6988fe1
eunchae <kec0130@gmail.com>
2022-12-01 13:12:00 +0900

    Feat: Add Pokemon types

--- types/pokemon.ts
+++ types/pokemon.ts
@@ -1,16 +1,97 @@
 export interface Pokemon {
   id: number;
   name: string;
+  base_experience: number;
+  height: number;
+  is_default: boolean;
+  order: number;
+  weight: number;
+  abilities: PokemonAbility[];
+  forms: NamedAPIResource[];
+  game_indices: VersionGameIndex[];
+  held_items: PokemonHeldItem[];
+  location_area_encounters: string;
+  moves: PokemonMove[];
+  past_types: PokemonTypePast[];
   sprites: PokemonSprites;
+  species: NamedAPIResource;
+  stats: PokemonStat[];
+  types: PokemonType[];
 }
 
-export interface PokemonSprites {
+interface NamedAPIResource {
+  name: string;
+  url: string;
+}
+
+interface PokemonAbility {
+  is_hidden: boolean;
+  slot: number;
+  ability: NamedAPIResource;
+}
+
+interface PokemonType {
+  slot: number;
+  type: NamedAPIResource;
+}
+
+interface PokemonTypePast {
+  generation: NamedAPIResource;
+  types: PokemonType[];
+}
+
+interface PokemonHeldItem {
+  item: NamedAPIResource;
+  version_details: PokemonHeldItemVersion[];
+}
+
+interface PokemonHeldItemVersion {
+  version: NamedAPIResource;
+  rarity: number;
+}
+
+interface PokemonMove {
+  move: NamedAPIResource;
+  version_group_details: PokemonMoveVersion[];
+}
+
+interface PokemonMoveVersion {
+  move_learn_method: NamedAPIResource;
+  version_group: NamedAPIResource;
+  level_learned_at: number;
+}
+
+interface PokemonStat {
+  stat: NamedAPIResource;
+  effort: number;
+  base_stat: number;
+}
+
+interface PokemonSprites extends PokemonSpritesFront, PokemonSpritesBack {
+  other: PokemonSpritesOther;
+}
+
+interface PokemonSpritesFront {
   front_default: string;
+  front_female: string | null;
   front_shiny: string;
-  front_female: string;
-  front_shiny_female: string;
+  front_shiny_female: string | null;
+}
+
+interface PokemonSpritesBack {
   back_default: string;
+  back_female: string | null;
   back_shiny: string;
-  back_female: string;
-  back_shiny_female: string;
+  back_shiny_female: string | null;
+}
+
+interface PokemonSpritesOther {
+  dream_world: Pick<PokemonSpritesFront, 'front_default' | 'front_female'>;
+  home: PokemonSpritesFront;
+  'official-artwork': Pick<PokemonSpritesFront, 'front_default'>;
+}
+
+interface VersionGameIndex {
+  game_index: number;
+  version: NamedAPIResource;
 }
